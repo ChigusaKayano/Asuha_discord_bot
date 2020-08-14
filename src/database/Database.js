7237a8a85f9880477a890abcc95c0acd757c07e6
Florian Lepage <Chigusa.Kayano@outlook.com>
2020-08-14 15:06:07 +0200

    Add Database.js class
    Add Database.js
    
    Used as reference to all querys

---
+++ src/database/Database.js
@@ -0,0 +1,24 @@
+//Database.js//
+const mysql = require("mysql2");
+
+class Database {
+    constructor(token) {
+        this.dbUsername = token.database.username;
+        this.dbDatabase = token.database.database;
+        this.dbPassword = token.database.password;
+        this.dbServer = token.database.server;
+        this.dbPort = token.database.port;
+    }
+    connection() {
+        return mysql.createConnection({
+            user: this.dbUsername,
+            database: this.dbDatabase,
+            password: this.dbPassword,
+            host: this.dbServer,
+            port: this.dbPort
+        })
+    }
+
+module.exports = {
+    Database
+}
