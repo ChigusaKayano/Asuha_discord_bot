e3658dd9379544d29552176574d7e5909b2259a2
Florian Lepage <Chigusa.Kayano@outlook.com>
2020-08-19 00:56:37 +0200

    Update Clear.js
    Fix commit revert error

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ const {Database} = require("./database/Database.js");
 //Administration related imports
 const {ModerationRole} = require("./action/administration/ModerationRole.js");
 //Moderation related imports
-const {Clear} = require("./action/moderation/Clear.js");
+const {Clear, ClearReact} = require("./action/moderation/Clear.js");
+//const {Mute} = require("./action/moderation/Mute.js");
 //event listener
 client.on("ready", () => {
     let time = `${new Date(Date.now()).getHours()}:${new Date(Date.now()).getMinutes()}:${new Date(Date.now()).getSeconds()}`
@@ -23,7 +24,10 @@ client.on("ready", () => {
 client.on("message", (message) => {
     new ModerationRole(message, config, client, token).redirect();
     new Clear(message, config, client, token).command();
+    //new Mute(message, config, client, token).redirect();
 })
-
+client.on("messageReactionAdd", (MessageReaction, User) => {
+    new ClearReact(MessageReaction, User, config, client, token).react();
+});
 
 client.login(token.discord.token);
