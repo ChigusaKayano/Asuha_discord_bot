0eea2770e32d497cc0d49823491dc5513d4aa3b2
Florian Lepage <Chigusa.Kayano@outlook.com>
2020-08-19 19:10:53 +0200

    Update Mute.js
    Update Mute.js:
    Add unmute command
    Add help commande related to mute commands
    
    Update en-US.json:
    text updated related to mute commands

--- src/action/moderation/Mute.js
+++ src/action/moderation/Mute.js
@@ -17,14 +17,17 @@ class Mute {
         if(this.message.author.id !== this.client.user.id) {
             switch (args[0].toLowerCase()) {
                 case this.prefix + "mute":
-                    this.mute();
+                    if(args[1].toLowerCase() === "help") {
+                        this.help();
+                    } else {
+                        this.mute();
+                    }
                     break;
                 case this.prefix + "tempmute":
                     this.tempMute();
                     break;
                 case this.prefix + "unmute":
-                    break;
-                case this.prefix + "help":
+                    this.unmute();
                     break;
             }
         }
@@ -184,6 +187,7 @@ class Mute {
                                                             this.message.guild.member(userId).roles.add(rows[0].value).then().catch(console.error);
                                                             db.connection().query(`SELECT value FROM setting_${this.message.guild.id} WHERE type = 'restriction'`, (err, rows) => {
                                                                 if (err) throw err;
+                                                                let setting = rows;
                                                                 this.message.delete().then().catch();
                                                                 let info = [userId, this.message.author.id, reason, args[2]];
                                                                 db.connection().query(`INSERT INTO mute_${this.message.guild.id} (userId, authorId, reason, muteFor) VALUES ("${info[0]}", "${info[1]}", "${info[2]}", "${info[3]}")`, (err, rows) => {
@@ -201,30 +205,29 @@ class Mute {
                                                                         }).catch(console.error);
                                                                     }
                                                                 });
+                                                                console.log(setting);
                                                                 console.log(`[${time}] '${this.message.author.tag}' has mute '${this.message.guild.member(userId).user.tag}' for ${info[3]} minutes because: ${info[2]}`);
-                                                            });
-                                                            setTimeout(() => {
-                                                                this.message.guild.member(userId).roles.remove(rows[0].value).then().catch(console.error);
-                                                                new Giphy(this.token.giphy.token).searchGif("kpop heart").then(content => {
-                                                                    let gifUrl = `https://media.giphy.com/media/${content.data[0].id}/giphy.gif`;
-                                                                    db.connection().query(`SELECT value FROM setting_${this.message.guild.id} WHERE type = 'restriction'`, (err, rows) => {
-                                                                        if(err) throw err;
-                                                                        let setting = rows;
-                                                                        db.connection().query(`SELECT * FROM mute_${this.message.guild.id} WHERE userId = '${userId}'`, (err, rows) => {
-                                                                            if(err) throw err;
-                                                                            if (setting.length >= 1) {
-                                                                                let channel = this.message.guild.channels.find(channel => channel.id === setting[0].id);
-                                                                                channel.send(this.embed(4, rows[rows.length - 1], language)).then().catch(console.error);
-                                                                            } else {
-                                                                                this.message.channel.send(this.embed(4, rows[rows.length - 1], language, gifUrl)).then(message => {
-                                                                                    message.channel.send(language.noteMessage[0].replace("PREFIX", this.prefix)).then(message => message.delete({timeout: 10000})).catch(console.error);
-                                                                                }).catch(console.error);
-                                                                            }
+                                                                setTimeout(() => {
+                                                                    if(this.message.guild.member(userId).roles.cache.some(role => role.id === setting[0].value)) {
+                                                                        this.message.guild.member(userId).roles.remove(rows[0].value).then().catch(console.error);
+                                                                        new Giphy(this.token.giphy.token).searchGif("kpop heart").then(content => {
+                                                                            let gifUrl = `https://media.giphy.com/media/${content.data[0].id}/giphy.gif`;
+                                                                            db.connection().query(`SELECT * FROM mute_${this.message.guild.id} WHERE userId = '${userId}'`, (err, rows) => {
+                                                                                if (err) throw err;
+                                                                                if (setting.length >= 1) {
+                                                                                    let channel = this.message.guild.channels.find(channel => channel.id === setting[0].value);
+                                                                                    channel.send(this.embed(4, rows[rows.length - 1], language)).then().catch(console.error);
+                                                                                } else {
+                                                                                    this.message.channel.send(this.embed(4, rows[rows.length - 1], language, gifUrl)).then(message => {
+                                                                                        message.channel.send(language.noteMessage[0].replace("PREFIX", this.prefix)).then(message => message.delete({timeout: 10000})).catch(console.error);
+                                                                                    }).catch(console.error);
+                                                                                }
+                                                                            });
+                                                                            console.log(`[${time}] The mute duration time has expired. '${this.message.guild.member(userId).user.tag}' has been un muted.}`);
                                                                         });
-                                                                        console.log(`[${time}] The mute duration time has expired. '${this.message.guild.member(userId).user.tag}' has been un muted.}`);
-                                                                    });
-                                                                });
-                                                            }, args[2] * 60000);
+                                                                    }
+                                                                }, args[2] * 60000);
+                                                            });
                                                         } else { // return a message error when the reason is containing "
                                                             this.message.delete().then().catch();
                                                             this.message.channel.send(language.messageError[8].replace("LETTER", '"')).then(message => message.delete({timeout: 10000})).catch(console.error);
@@ -280,6 +283,114 @@ class Mute {
             });
         });
     }
+    unmute() {
+        let args = this.message.content.slice().split(/ /); // init the var which contain the message/command args
+        let time = `${new Date(Date.now()).getHours()}:${new Date(Date.now()).getMinutes()}:${new Date(Date.now()).getSeconds()}`;
+        let db = new Database(this.token);
+        db.connection().query(`SELECT roleId FROM moderation_${this.message.guild.id}`, (err, rows) => {
+            if (err) throw err;
+            let Role = rows;
+            db.connection().query(`SELECT value FROM setting_${this.message.guild.id} WHERE type = 'language'`, (err, rows) => {
+                if (err) throw err;
+                let language = JSON.parse(fs.readFileSync(`./languages/${rows[0].value}.json`)).commands.moderation.mute.unmute;
+                if (Role.length >= 1) {
+                    let answer = false;
+                    for (let i = 0; Role.length > i; i++) {
+                        if (this.message.guild.member(this.message.author.id).roles.cache.some(role => role.id === Role[i].roleId)) {
+                            answer = true;
+                        }
+                    }
+                    if (answer) {
+                        db.connection().query(`SELECT value FROM setting_${this.message.guild.id} WHERE type = "muteRole"`, (err, rows) => {
+                            if (err) throw err;
+                            if (rows.length >= 1) {
+                                if (typeof args[1] !== "undefined") {
+                                    let userId;
+                                    if (args[1].charAt(0) === "<") {
+                                        userId = this.message.mentions.members.first().id;
+                                    } else {
+                                        userId = args[1];
+                                    }
+                                    if (userId.length === 18) {
+                                        if (userId !== this.message.author.id) {
+                                            if (this.message.guild.member(userId).roles.cache.some(role => role.id === rows[0].value)) {
+                                                this.message.guild.member(userId).roles.remove(rows[0].value).then().catch(console.error);
+                                                this.message.delete().then().catch(console.error);
+                                                db.connection().query(`SELECT value FROM setting_${this.message.guild.id} WHERE type = 'restriction'`, (err, rows) => {
+                                                    if (err) throw err;
+                                                    let setting = rows;
+                                                    db.connection().query(`SELECT * FROM mute_${this.message.guild.id} WHERE userId = '${userId}'`, (err, rows) => {
+                                                        if(err) throw err;
+                                                        new Giphy(this.token.giphy.token).searchGif("kpop heart").then(content => {
+                                                            let gifUrl = `https://media.giphy.com/media/${content.data[0].id}/giphy.gif`;
+                                                            if (setting.length >= 1) {
+                                                                let channel = this.message.guild.channels.find(channel => channel.id === setting[0].value);
+                                                                channel.send(this.embed(4, rows[rows.length - 1], language)).then().catch(console.error);
+                                                            } else {
+                                                                this.message.channel.send(this.embed(4, rows[rows.length - 1], language, gifUrl)).then(message => {
+                                                                    message.channel.send(language.noteMessage[0].replace("PREFIX", this.prefix)).then(message => message.delete({timeout: 10000})).catch(console.error);
+                                                                }).catch(console.error);
+                                                            }
+                                                        });
+                                                    });
+                                                    console.log(`[${time}] '${this.message.author.tag}' has unmute '${this.message.guild.member(userId).user.tag}`);
+                                                });
+                                            } else { // return a message if the targeted user is already muted.
+                                                this.message.delete().then().catch();
+                                                this.message.channel.send(language.messageError[5]).then(message => message.delete({timeout: 10000})).catch(console.error);
+                                            }
+                                        } else { // return a message error if the use targeted is the command caller.
+                                            this.message.delete().then().catch();
+                                            this.message.channel.send(language.messageError[4]).then(message => message.delete({timeout: 10000})).catch(console.error);
+                                        }
+                                    } else { // return a message error when the specified user is not valid.
+                                        this.message.delete().then().catch();
+                                        this.message.channel.send(language.messageError[3]).then(message => message.delete({timeout: 10000})).catch(console.error);
+                                    }
+                                } else { // return a message error when the person that used the command did not specified the user
+                                    this.message.delete().then().catch();
+                                    this.message.channel.send(language.messageError[2]).then(message => message.delete({timeout: 10000})).catch(console.error);
+                                }
+                            } else { // return a message error of there is not role muted on the server.
+                                this.message.delete().then().catch();
+                                this.message.channel.send(this.embed(2, null, language)).then(message => {
+                                    db.connection().query(`SELECT id FROM msgId_${this.message.guild.id} WHERE type = 'mute'`, (err, rows) => {
+                                        if (err) throw err;
+                                        let query;
+                                        if (rows.length >= 1) {
+                                            query = `UPDATE msgId_${this.message.guild.id} SET id = '${message.id}' WHERE type = 'mute'`;
+                                        } else {
+                                            query = `INSERT INTO msgId_${this.message.guild.id} (type, id) VALUES ('mute', '${message.id}')`;
+                                        }
+                                        db.connection().query(query, (err) => {
+                                            if (err) throw err
+                                        });
+                                        message.react("✅").then().catch(console.error);
+                                    });
+                                }).catch(console.error);
+                            }
+                        })
+                    } else { // return message error when the person that used the command is not a moderator
+                        this.message.delete().then().catch(console.error);
+                        this.message.channel.send(language.messageError[0]).then(message => message.delete({timeout: 10000})).catch(console.error);
+                    }
+                }
+            });
+        });
+    }
+    help() {
+        let db = new Database(this.token);
+        let time = `${new Date(Date.now()).getHours()}:${new Date(Date.now()).getMinutes()}:${new Date(Date.now()).getSeconds()}`;
+        if(this.message.author !== this.client.user.id) {
+            db.connection().query(`SELECT value FROM setting_${this.message.guild.id} WHERE type = 'language'`, (err, rows) => {
+                if (err) throw err;
+                let language = JSON.parse(fs.readFileSync(`./languages/${rows[0].value}.json`)).commands.moderation.mute.help;
+                this.message.delete().then().catch(console.error);
+                this.message.channel.send(this.embed(5, null, language)).then().catch(console.error);
+                console.log(`[${time}] ${this.message.author.tag} just asked for help with commands related to mute.`);
+            });
+        }
+    }
     embed(Case, info=null, language, image=null) {
         switch (Case) {
             case 1:
@@ -332,6 +443,20 @@ class Mute {
                     )
                     .setImage(image)
                     .setColor("GREEN")
+                    .setFooter(`@florian_kyn`, "https://i.imgur.com/2XRrIuv.png")
+                    .setTimestamp();
+                break;
+            case 5:
+                return new discord.MessageEmbed()
+                    .setAuthor(this.message.guild.name, this.client.user.avatarURL())
+                    .setThumbnail(this.message.guild.member(this.message.author.id).user.avatarURL())
+                    .addFields(
+                        {name: "`" + `${this.prefix}tempmute <@mention/id> <duration> <reason>` + "`", value: language.commandsDesc[1], inline: false},
+                        {name: "`" + `${this.prefix}mute <@mention/id> <reason>` + "`", value: language.commandsDesc[0], inline: false},
+                        {name: "`" + `${this.prefix}unmute <@mention/id>` + "`", value: language.commandsDesc[2], inline: false},
+                        {name: "`" + `${this.prefix}mute help` + "`", value: language.commandsDesc[3], inline: false}
+                    )
+                    .setColor("YELLOW")
                     .setFooter(`@florian_kyn`, "https://i.imgur.com/2XRrIuv.png")
                     .setTimestamp();
                 break;
